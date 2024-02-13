ad750ab893bb280e0c47e060b69a5040f0e27757
Eduardo De Souza Santos <119471845+Eduardoss45@users.noreply.github.com>
2024-02-13 20:24:42 -0300

    Finalização do projeto movieslib.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,38 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  };
   return (
     <div>
       <nav id="navbar">
         <h2>
-          <Link to="/">MoviesLib</Link>
+          <Link to="/">
+            <BiCameraMovie />
+            MoviesLib
+          </Link>
         </h2>
-        <form>
-            <input type="text" placeholder="busque um filme" />
-            <button type="submit"></button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="busque um filme"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <button type="submit">
+            <BiSearchAlt2 />
+          </button>
         </form>
       </nav>
     </div>
