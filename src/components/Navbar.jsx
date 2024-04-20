5b81f4e24ff01545cc9ac8a7c72881a28c39a6ce
Eduardo De Souza Santos <119471845+Eduardoss45@users.noreply.github.com>
2024-04-20 12:16:19 -0300

    Correção do card

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  };
+  return (
+    <div>
+      <nav id="navbar">
+        <h2>
+          <Link to="/">
+            <BiCameraMovie />
+            <span className="visible">MoviesLib</span>
+          </Link>
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="busque um filme"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <button type="submit">
+            <BiSearchAlt2 />
+          </button>
+        </form>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
